c0e12cfef2be1dc20c92dfbddb3f05a91f44bf71
Nils Ove Tendenes <nils.ove@tendenes.com>
2024-10-01 16:01:37 +0200

    chore: add loggers in service and controller

--- src/main/kotlin/no/digdir/fdk/statistics/controller/StatisticsController.kt
+++ src/main/kotlin/no/digdir/fdk/statistics/controller/StatisticsController.kt
@@ -5,6 +5,8 @@ import no.digdir.fdk.statistics.model.TimeSeriesPoint
 import no.digdir.fdk.statistics.model.TimeSeriesRequest
 import no.digdir.fdk.statistics.service.EndpointPermissions
 import no.digdir.fdk.statistics.service.StatisticsService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -21,12 +23,16 @@ class StatisticsController(
     private val statisticsService: StatisticsService,
     private val endpointPermissions: EndpointPermissions
 ) {
+    private val logger: Logger = LoggerFactory.getLogger(StatisticsController::class.java)
 
     @PostMapping(value = ["/time-series"])
     fun timeSeries(@RequestBody req: TimeSeriesRequest?): ResponseEntity<List<TimeSeriesPoint>>  =
         try {
             ResponseEntity(statisticsService.timeSeries(req ?: TimeSeriesRequest()), HttpStatus.OK)
-        } catch (ex: ResponseStatusException) { ResponseEntity(ex.statusCode) }
+        } catch (ex: ResponseStatusException) {
+            logger.debug("Time series failed with code {} and reason {}", ex.statusCode, ex.reason)
+            ResponseEntity(ex.statusCode)
+        }
 
     @PostMapping(value = ["/calculate-latest"])
     fun calculateLatest(
@@ -34,8 +40,13 @@ class StatisticsController(
         @RequestBody req: CalculationRequest
     ): ResponseEntity<Unit> =
         if (endpointPermissions.hasAdminPermission(jwt)) {
-            statisticsService.calculateLatest(req)
-            ResponseEntity(HttpStatus.OK)
+            try {
+                statisticsService.calculateLatest(req)
+                ResponseEntity(HttpStatus.OK)
+            } catch (ex: ResponseStatusException) {
+                logger.debug("Calculate latest failed with code {} and reason {}", ex.statusCode, ex.reason)
+                ResponseEntity(ex.statusCode)
+            }
         } else ResponseEntity(HttpStatus.FORBIDDEN)
 
 }
